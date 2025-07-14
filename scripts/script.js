6b56883ed61df605ce72c594601bb08d7287aaff
ElPopo18 <70975283+ElPopo18@users.noreply.github.com>
2025-07-14 15:18:12 -0400

    Add the option to change the number of squares
    This will add the option to change the number of squares inside the grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    function assignGrid(){
+        let message = window.prompt("Enter the size of the grid (between 1 and 100)");
+        let totalDivs = parseInt(message);
+
+        totalDivs = Math.pow(totalDivs, 2);
+
+        createGrid(totalDivs, message);
+    }
+
     let container = document.querySelector(".container")
+    const button = document.querySelector(".button");
 
     let grid = Math.pow(16, 2);
 
-    for (let i = 0; i < grid; i++){
-        const item = document.createElement("div");
+    function createGrid(totalDivs, size){
+        size = 900/size;
+        let size2 = `${size}px`;
+        
+        for (let i = 0; i < totalDivs; i++){
+            const item = document.createElement("div");
 
-        item.classList.add("item", "item-" + i);
-        container.appendChild(item);
+            item.classList.add("item", "item-" + i);
+            container.appendChild(item);
 
-        item.addEventListener("mouseover", (e) => {
-        item.style.backgroundColor = "black";
+            item.style.border = '1px solid black';
+            item.style.width = size2;
+            item.style.height = size2;
+
+            item.addEventListener("mouseover", (e) => {
+            item.style.backgroundColor = "black";
+            console.log(item);
         })
+        }
     }
 
-
+    button.addEventListener("click", assignGrid);
 
 })
